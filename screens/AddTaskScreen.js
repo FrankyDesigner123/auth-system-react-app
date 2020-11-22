ca8bed251c68f393ce45da1dcee3c7a6dad38606
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-11-22 23:03:53 +0100

    Adding some UI on TaskDetails Screen

--- screens/AddTaskScreen.js
+++ screens/AddTaskScreen.js
@@ -1,14 +1,62 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import {
+	StyleSheet,
+	View,
+	Text,
+	ScrollView,
+	TextInput,
+	Button,
+} from 'react-native';
 
 const AddTaskScreen = () => {
 	return (
-		<View>
-			<Text>Add a Task Screen</Text>
-		</View>
+		<ScrollView>
+			<View style={styles.form}>
+				<View style={styles.formGroup}>
+					<Text style={styles.label}>Title:</Text>
+					<TextInput style={styles.inputField} />
+				</View>
+
+				<View style={styles.formGroup}>
+					<Text style={styles.label}>Course:</Text>
+					<TextInput style={styles.inputField} />
+				</View>
+
+				<View style={styles.formGroup}>
+					<Text style={styles.label}>Description:</Text>
+					<TextInput style={styles.inputField} />
+				</View>
+
+				<View style={styles.buttonContainer}>
+					<Button title="Add Task" />
+				</View>
+			</View>
+		</ScrollView>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	form: {
+		margin: 20,
+		backgroundColor: '#fff',
+		padding: 20,
+		borderRadius: 10,
+	},
+	formGroup: {
+		width: '100%',
+	},
+	label: {
+		marginVertical: 10,
+	},
+	inputField: {
+		paddingHorizontal: 2,
+		paddingVertical: 8,
+		borderBottomColor: '#ccc',
+		borderBottomWidth: 1,
+	},
+	buttonContainer: {
+		marginTop: 20,
+	},
+});
 
 export default AddTaskScreen;
--- screens/TaskDetailsScreen.js
+++ screens/TaskDetailsScreen.js
@@ -1,14 +1,53 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Platform } from 'react-native';
 
 const TaskDetailsScreen = () => {
 	return (
-		<View>
-			<Text>Task Detail Screen</Text>
-		</View>
+		<ScrollView>
+			<View style={styles.container}>
+				<View style={styles.heading}>
+					<Text style={styles.title}>Dummy title</Text>
+				</View>
+
+				<View style={styles.group}>
+					<Text style={styles.label}>Course:</Text>
+					<Text style={styles.value}>Web Design</Text>
+				</View>
+
+				<View style={styles.group}>
+					<Text style={styles.label}>Description:</Text>
+					<Text style={styles.value}>Some dummy description....</Text>
+				</View>
+			</View>
+		</ScrollView>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		marginVertical: 20,
+	},
+	heading: {
+		marginHorizontal: 20,
+		marginBottom: 10,
+	},
+	title: {
+		fontSize: 24,
+	},
+	group: {
+		marginHorizontal: 20,
+		marginVertical: 10,
+		flexDirection: 'row',
+	},
+	label: {
+		fontSize: 18,
+	},
+	value: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		flexShrink: 1,
+	},
+});
 
 export default TaskDetailsScreen;
