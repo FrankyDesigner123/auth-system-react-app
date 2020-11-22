6877b87f99d05d6a42a2db7c7b873856596c58b1
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-11-22 18:09:00 +0100

    Decoding tokens using jwt-decode

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,14 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const jwtDecode = require('jwt-decode');
+
 const HomeScreen = (props) => {
+	const [fullName, setFullName] = useState('');
+	const [email, setEmail] = useState('');
+	const [role, setRole] = useState('');
+
 	const loadProfile = async () => {
 		// fetch token
 		const token = await AsyncStorage.getItem('token');
@@ -10,7 +16,20 @@ const HomeScreen = (props) => {
 		if (!token) {
 			props.navigation.navigate('Login');
 		}
-		console.log(token);
+		// decoding the token
+		const decoded = jwtDecode(token);
+		setFullName(decoded.fullName);
+		setEmail(decoded.email);
+		setRole(decoded.role);
+		console.log(decoded);
+	};
+
+	const logout = (props) => {
+		AsyncStorage.removeItem('token')
+			.then(() => {
+				props.navigation.replace('Login');
+			})
+			.catch((err) => console.log(err));
 	};
 
 	useEffect(() => {
@@ -18,12 +37,39 @@ const HomeScreen = (props) => {
 	});
 
 	return (
-		<View>
-			<Text>Login Screen</Text>
+		<View style={styles.container}>
+			<View>
+				<Text style={styles.text}>Welcome {fullName ? fullName : ''}</Text>
+			</View>
+
+			<View>
+				<Text style={styles.text}>Your Email: {email ? email : ''}</Text>
+			</View>
+
+			<View>
+				<Text style={styles.text}>Role: {role ? role : ''}</Text>
+			</View>
+
+			<View>
+				<Button
+					title="Log out"
+					onPress={() => {
+						logout(props);
+					}}
+				/>
+			</View>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 40,
+	},
+	text: {
+		fontSize: 22,
+	},
+});
 
 export default HomeScreen;
